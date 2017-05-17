724b48e25432f41606e4e332075de097ffef6519
shogunatealex <alexanderson234@gmail.com>
2017-05-17 17:10:33 -0500

    Added Ability to Control Html on site
    Can now alter HTML on Webpage

--- highlightKanji.js
+++ highlightKanji.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
         alert('the DOM is ready');
     });
+var testText = $('p:last').text();
+$('div, li, ol, p, h1, h2, h3, h4, h5, span, a, th, td, code, strong, em').css("color", "red");
+
+var textLibrary = $('li, p, h1, h2, h3, h4, h5, span').text();
+console.log(textLibrary);
+
+//console.log(textLibrary);
