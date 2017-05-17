43be87ae6aacfb4650104b1129abe4cbfbc365fc
shogunatealex <alexanderson234@gmail.com>
2017-05-17 16:01:33 -0500

    Created highlight script
    Here is the script that will be used to identify and change kanji on the
    page.

---
+++ highlightKanji.js
@@ -0,0 +1,6 @@
+include('http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js', function() {
+    $(document).ready(function() {
+      console.log('ready');
+        alert('the DOM is ready');
+    });
+});
