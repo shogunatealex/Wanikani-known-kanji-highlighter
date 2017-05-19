16736633fdf86fb0630442e74fbf1245c8fa5b5f
shogunatealex <alexanderson234@gmail.com>
2017-05-18 21:58:01 -0500

    Added text replacement abiity
    Successfully linked the Wanikani library to the webpage that searches
    through the library and replaces the kanji with the new color kanji

--- highlightKanji.js
+++ highlightKanji.js
@@ -20,22 +20,23 @@ function callWanikani (callback){
     // if no vocabulary dictionary has been saved, start saving it.
     if(storage["storage-initial"] == 0){
       // call the wanikani server to get the users vocab list up to their level
-      $.getJSON("https://www.wanikani.com/api/user/4a5d0dbcc23712212e7684fe99935275/vocabulary", function(data){
+      $.getJSON("https://www.wanikani.com/api/user/4a5d0dbcc23712212e7684fe99935275/kanji", function(data){
         console.log(data);
         // console.log(data.requested_information.general);
         // take each item and strip it to its CHARACTER and SRS level
-        $.each(data.requested_information.general,function(index){
-          var char = data.requested_information.general[index].character;
-          var hasData = data.requested_information.general[index].user_specific;
+        $.each(data.requested_information,function(index){
+          var char = data.requested_information[index].character;
+          var hasData = data.requested_information[index].user_specific;
           // checks if user hasn't seen the word yet. In that case can't call user_specific as it is Null
           if(!hasData){
             console.log("here");
           }
           //push to vocab list
-          vocabList.push({character: data.requested_information.general[index].character,
+          vocabList.push({character: data.requested_information[index].character,
                           srs:  !hasData? "null" : hasData.srs});
         });
         // call to function that sves this data.
+        console.log("Length is" + vocabList.length);
         callback(vocabList);
       });
 
@@ -57,6 +58,7 @@ function callWanikani (callback){
 function saveData(vocabList, callback){
   // will receive this only if the data doesn't need to be saved
   if(vocabList.length == 0){
+    console.log("data not saved");
     // move on to searching the data
     callback();
   }
@@ -75,12 +77,13 @@ function searchData () {
   // retrieve the data saved locally
   chrome.storage.local.get("vocabulary-list", function(tree){
     console.log(tree);
-    // var specificTree = tree.filter(function(v){
-    //   return v["character"] == "に";
-    // })
-    // console.log(specificTree);
+    console.log("hello" == "hello");
+    var specificTree = tree["vocabulary-list"].filter(function(v){
+      return v["character"] == "食べる";
+    })
+    console.log(specificTree);
     // grab all of the html pages text
-    var DOMlibrary = $('li, p, h1, h2, h3, h4, h5');
+    var DOMlibrary = $('span, ul, p, h1, h2, h3, h4, h5');
     console.log(typeof(DOMlibrary));
     console.log(DOMlibrary);
     var textLibrary = [];
@@ -91,42 +94,61 @@ function searchData () {
       $(this).addClass(uniqueClass);
       // takes DOM object and grabs it's text
       var tempLibrary = $(this).text();
-      // splits that text up into words
-      var splitTempLibrary = tempLibrary.split(" "); //Here will be the split function for JAPANESE
+      // splits that text up into words (right now splits by every character)
+      var splitTempLibrary = tempLibrary.split(''); //Here will be the split function for JAPANESE
       // push these objects into an array that holds each text item and it's element
       for(var i = 0; i < splitTempLibrary.length; i++){
         // cleans strings of punctuation
-        var toPush = splitTempLibrary[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+        var toPush = splitTempLibrary[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\n\r、]/g,"");
+        if (toPush == "" || (toPush.toLowerCase() != toPush.toUpperCase()) || toPush == " "){
+
+        }
+        else{
+          console.log(toPush);
+          textObjectArray.push({text: toPush, class: uniqueClass});
+        }
         // should be replaced with Red-black tree
-        textObjectArray.push({text: toPush, class: uniqueClass});
       }
       textLibrary.push($(this).text());
       counter += 1;
      });
+     console.log(textObjectArray);
+     tree["vocabulary-list"].forEach(function(word){
+       var temp = textObjectArray.filter(function(v){
+         return v["text"] == word.character;
+       })
+       if(temp.length > 0){
+         console.log("something here");
+         //console.log(temp);
+         temp.forEach(function(match){
 
-    var objects = textObjectArray.filter(function(v){
-      // grabs all objects with text of japanese
-      return v["text"].toLowerCase() == "journey";
-    });
-    objects.forEach(function(match){
+           var currentElement = $("." + match.class);
+           //console.log(match.text);
+           var tempText = currentElement.text();
+           tempText = tempText.replace(match.text,"<span style=\"color:blue\">" + match.text + "</span>");
+           //console.log(tempText);
+           if(currentElement.is('li')){
+             currentElement.children().html(tempText);
+           }
+           else{
+             currentElement.html(tempText);
+           }
+           // changes their overarching classes to blue;
 
-      var currentElement = $("." + match.class);
-      console.log(match.text);
-      var tempText = currentElement.text();
-      tempText = tempText.replace(match.text,"<span style=\"color:blue\">" + match.text + "</span>");
-      console.log(tempText);
-      if(currentElement.is('li')){
-        currentElement.children().html(tempText);
-      }
-      else{
-        currentElement.html(tempText);
-      }
-      // changes their overarching classes to blue;
+            //$("." + match.class).css("color","blue");
+            //$("." + match.class).children().css("color","blue");
+           //console.log($("." + match.class));
+         })
+       }
+       else{
+         console.log("nothing here");
+       }
+     })
+    // var objects = textObjectArray.filter(function(v){
+    //   // grabs all objects with text of japanese
+    //   return v["text"].toLowerCase() == "journey";
+    // });
 
-       //$("." + match.class).css("color","blue");
-       //$("." + match.class).children().css("color","blue");
-      //console.log($("." + match.class));
-    })
 
   })
 };
