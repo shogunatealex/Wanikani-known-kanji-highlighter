56264b98784e0412cd1130f668ce845927aa2c8f
Alex Anderson <alexanderson234@gmail.com>
2017-11-14 10:22:29 -0600

    Reduced MatchText Calls
    Reduced matchtext calls from total number of learned kanji down to a
    constant 5 times. This greatly reduced the amount of time needed to run
    on large web pages from 34 seconds down to .8 seconds. A pretty good
    optimization if I do say so myself.

--- highlightKanji.js
+++ highlightKanji.js
@@ -1,23 +1,13 @@
 $(document).ready(function() {
-  console.log("here");
   callWanikani(function(x){
-    console.log('here');
     saveData(x,function(){
-      console.log("here2");
       searchData();
     })
   });
     });
-//var testText = $('p:last').text();
-//$('div, li, ol, p, h1, h2, h3, h4, h5, span, a, th, td, code, strong, em').css("color", "red");
-
-
 chrome.storage.sync.get("storage-initial", function(){
       console.log("Storage value initialized");
     });
-
-
-
 
 // gets the vocab tree and stores it if it hasn't already been stored
 function callWanikani (callback){
@@ -79,7 +69,7 @@ function saveData(vocabList, callback){
   }
 }
 
-
+// called in function from stackoverflow to find text without disturbing dom
 var matchText = function(node, regex, callback, excludeElements) {
 
     excludeElements || (excludeElements = ['script', 'style', 'iframe', 'canvas']);
@@ -123,214 +113,67 @@ var matchText = function(node, regex, callback, excludeElements) {
 };
 
 function searchData () {
+  var apprentice,guru,master,enlighten,burned;
+  // Create the start of the regex expression used to replace the known kanji
+  apprentice = guru = master = enlighten = burned = "[";
+
+  // grab the list of kanji that we know and have stored in chrome
   chrome.storage.local.get("vocabulary-list", function(tree){
     console.time('matchText');
-    console.log(tree);
-    var html = ($("body")[0].innerHTML);
+    // go through list and seperate text out into regex expressions
     for (var x=0; x < tree["vocabulary-list"].length; x++){
       searchTerm = tree["vocabulary-list"][x].character;
       var memorizeLevel = tree["vocabulary-list"][x].srs;
-      if(html.search(searchTerm) == -1){
-        console.log("here");
-        continue;
+      // gets filtered into different regex expressions, think harry potter
+      if(memorizeLevel == "apprentice"){
+        apprentice += searchTerm;
       }
-      matchText(document.getElementsByTagName("body")[0], new RegExp(searchTerm, "g"), function(node, match, offset) {
+      else if (memorizeLevel == "guru"){
+        guru += searchTerm;
+      }
+      else if (memorizeLevel == "master"){
+        master+= searchTerm;
+      }
+      else if (memorizeLevel == "enlighten"){
+        enlighten += searchTerm;
+      }
+      else if (memorizeLevel == "burned"){
+        burned += searchTerm;
+      }
+    } // end for
+    // close off regex expression
+    apprentice += "]";
+    guru += "]";
+    master += "]";
+    enlighten += "]";
+    burned += "]";
+    // put these regex expressions into array to go through
+    var kanjiList = [apprentice, guru, master, enlighten, burned];
+    // go through each level and run a regex replace on it
+    for(var i = 0; i < kanjiList.length; i++){
+      matchText(document.getElementsByTagName("body")[0], new RegExp(kanjiList[i], "g"), function(node, match, offset) {
           var span = document.createElement("span");
-          if(memorizeLevel == "apprentice"){
+          // give the replace kanji the appropriate color
+          if(i==0){
             span.className = "wanikani-apprentice";
           }
-          else if (memorizeLevel == "guru"){
+          else if (i==1){
             span.className = "wanikani-guru";
           }
-          else if (memorizeLevel == "master"){
+          else if (i==2){
             span.className = "wanikani-master";
           }
-          else if (memorizeLevel == "enlighten"){
+          else if (i==3){
             span.className = "wanikani-enlighten";
           }
-          else if (memorizeLevel == "burned"){
+          else if (i==4){
             span.className = "wanikani-burned";
           }
 
           span.textContent = match;
           return span;
-      });
-    }
+      }); // end matchText
+    } // end for
     console.timeEnd('matchText');
-
-  });
-
-
-//   console.timeEnd('matchText');
-//   function getTextNodesIn(node, includeWhitespaceNodes) {
-//     var textNodes = [], nonWhitespaceMatcher = /\S/;
-//
-//     function getTextNodes(node) {
-//         if (node.nodeType == 3) {
-//             if (includeWhitespaceNodes || nonWhitespaceMatcher.test(node.nodeValue)) {
-//                 textNodes.push(node);
-//             }
-//         } else {
-//             for (var i = 0, len = node.childNodes.length; i < len; ++i) {
-//                 getTextNodes(node.childNodes[i]);
-//             }
-//         }
-//     }
-//
-//     getTextNodes(node);
-//     return textNodes;
-// }
-//
-//
-//   console.log(getTextNodesIn($("body")[0]));
-  //retrieve the data saved locally,
-  // chrome.storage.local.get("vocabulary-list", function(tree){
-  //   // go through all text nodes
-  //   console.log(tree);
-  //   $.each(getTextNodesIn($("body")[0]), function(index, item){
-  //     //console.log(item.parentElement);
-  //     var u = item.textContent;
-  //     var tempU = "";
-  //     //console.log(u);
-  //     for(var i = 0; i < u.length; i++){
-  //       //console.log(u[i])
-  //       var specificTree = tree["vocabulary-list"].filter(function(v){
-  //         return v["character"] == u[i].toString();
-  //       }); // end function
-  //       if(specificTree.length > 0){
-  //         tempU += u[i];
-  //         var color;
-  //         if(specificTree[0].srs.toString() == "apprentice"){
-  //           tempU += '<span class="wanikani-apprentice" style=\"padding:0; margin: 0; display: inline\">';
-  //         }
-  //         else if(specificTree[0].srs.toString() == "guru"){
-  //           tempU += '<span class="wanikani-guru" style=\"padding:0; margin: 0; display: inline\">';
-  //         }
-  //         else if(specificTree[0].srs.toString() == "master"){
-  //           tempU += '<span class="wanikani-master" style=\"padding:0; margin: 0; display: inline\">';
-  //         }
-  //         else if(specificTree[0].srs.toString() == "enlighten"){
-  //           tempU += '<span class="wanikani-enlighten" style=\"padding:0; margin: 0; display: inline\">';
-  //         }
-  //         else if(specificTree[0].srs.toString() == "burned"){
-  //           tempU += '<span class="wanikani-burned" style=\"padding:0; margin: 0; display: inline\">';
-  //         }
-  //         tempU += specificTree[0].character + '</span>';
-  //
-  //       }// end if
-  //       else{
-  //         tempU += u[i];
-  //       }
-  //     }// end for
-  //     item.data = tempU;
-  //     console.log($(item.parentElement));
-  //
-  //   });
-  // });
-  //   //console.log(tree);
-  //   //console.log("hello" == "hello");
-  //   // grab all of the html pages text
-  //   //var DOMlibrary = $('tr, td, ruby, span, p, h1, h2, h3, h4, h5, a');
-  //   var temp = $('body').find("*").contents().each(function(){
-  //     if(this.nodeType == 3){
-  //       var u = this.nodeValue;
-  //       var tempU = "";
-  //       //console.log(u);
-  //       for(var i = 0; i < u.length; i++){
-  //         //console.log(u[i] + " " + i);
-  //         //u[i] = u[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\n\r、1234567890?)(,}{})]/g,"");
-  //         var checker = "[]().,;}{1234567890!@#$%^&*-_\n\r=+\"\'`~?.,<>。わらやまはなたさかありみひにちしきいるゆむふぬつすくうれめへねてせけえをろよもほのとそこおん ".split("");
-  //         //var checker = ["[", "]", "(", ")", ".", ",", ";", "}", "{", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0","#", "<", ":", "=", "!", "-", "_", "&", "@", "\"", "\'"];
-  //         if(u[i] == "" || (u[i].toLowerCase() != u[i].toUpperCase()) || u[i] == " " || checker.indexOf(u[i]) > -1){
-  //           tempU+= u[i];
-  //         }
-  //         else{
-  //         //console.log(u[i]);
-  //           //console.log(u[i]);
-  //           var specificTree = tree["vocabulary-list"].filter(function(v){
-  //             return v["character"] == u[i].toString();
-  //           }); // end function
-  //           if(specificTree.length > 0){
-  //             var replace = new RegExp(specificTree[0].character.toString(), "g");
-  //             //$(this).replaceWith(u.replace(replace,'|'));
-  //             var color;
-  //             if(specificTree[0].srs.toString() == "apprentice"){
-  //               color = "#F300A2";
-  //             }
-  //             else if(specificTree[0].srs.toString() == "guru"){
-  //               color ="#9F34B9";
-  //             }
-  //             else if(specificTree[0].srs.toString() == "master"){
-  //               color = "#4765E0";
-  //             }
-  //             else if(specificTree[0].srs.toString() == "enlighten"){
-  //               color ="#009CEA";
-  //             }
-  //             else if(specificTree[0].srs.toString() == "burned"){
-  //               color = "#B04A49";
-  //             }
-  //             tempU += '<span style=\"color:' + color + '; padding:0; margin: 0; display: inline\">' + specificTree[0].character + '</span>';
-  //             //$(this).replaceWith(tempU.replace(replace,'<span style=\"color:blue; padding:0; margin: 0; display: inline\">' + specificTree[0].character + '</span>'));
-  //             //console.log(u.length);
-  //           }
-  //           else{
-  //             tempU+=u[i];
-  //           }
-  //
-  //         } // end else
-  //       } // end for
-  //       $(this).replaceWith(tempU);
-  //       //console.log(tempU);
-  //     }
-  //   }); // end anon function
-  // }) // end chrome.storage.get
-}; //end search data
-
-
-
-
-
-
-
-
-
-
-//https://www.wanikani.com/api/user/4a5d0dbcc23712212e7684fe99935275/vocabulary/1
-//console.log(objects);
-
- //console.log(textObjectArray);
-
-
-
-
-
-
-// console.log(textLibrary);
-// var seperatedTextLibrary = [];
-// textLibrary.forEach(function(item){
-//   var temp = item.split(" ");
-//   for(var i = 0; i < temp.length; i++){
-//     if(temp[i] == "" || temp[i] == " "){
-//
-//     }
-//     else{
-//       seperatedTextLibrary.push(temp[i].toUpperCase());
-//     }
-//   }
-// });
-// if(seperatedTextLibrary.indexOf("JAPANESE".toUpperCase()) > -1){
-//   console.log("here");
-// }
-// console.log(seperatedTextLibrary);
-// var newTextLibrary = textLibrary.split(" ");
-// var x = 0;
-// console.log("\n\n\n");
-// console.log(newTextLibrary);
-// newTextLibrary.forEach(function(library){
-//   var temp = library.split(" ");
-//   for(var i = 0; i < temp.length; i++){
-//     x += 1;
-//     console.log(x);
-//   }
-// });
-//console.log(textLibrary);
+  }); //end chrome.storage.get
+} // end searchData
